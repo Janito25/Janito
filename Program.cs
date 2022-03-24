3626c0be7db3354f2fea9c46bca75cd2707f456e
Janito25 <102222204+Janito25@users.noreply.github.com>
2022-03-24 00:34:52 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Random rand = new Random();
+int numRand = rand.Next(5, 11);
+int horas = 0;
+
+for (int minutos = 1; minutos <= 60; minutos++)
+{
+
+    if (minutos.Equals(60))
+    {
+        horas++;
+        Console.WriteLine("Passou");
+        Console.WriteLine("São " + horas + " horas!");
+        minutos = 0;
+
+    }
+
+    if (horas.Equals(numRand))
+    {
+        Console.WriteLine("Passaram " + horas + " horas!");
+        break;
+    }
+
+}
+
---
+++ Program2.cs
@@ -0,0 +1,26 @@
+﻿
+string[] listaPalavras = new string[] {"addition", "fail", "zone", "isolation", "poison", "cutting", "court", "attitude", "bronze"};
+
+foreach(var palavra in listaPalavras)
+    Console.WriteLine(palavra);
+
+Console.WriteLine("Qual o tema que deseja pesquisar?");
+string tema = Console.ReadLine();
+
+
+void ordenarPalavras(string[] listaPalavras, string tema) {
+    StringComparer ordenar = StringComparer.CurrentCultureIgnoreCase;
+
+    Array.Sort(listaPalavras, ordenar);
+
+    Console.WriteLine();
+    Console.WriteLine("Ordem alfabética:");
+
+    for (int i = 0; i < listaPalavras.Length; i++)
+    {
+        Console.WriteLine(listaPalavras[i]);
+    }
+
+
+}
+
